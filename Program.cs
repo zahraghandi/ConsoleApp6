df4e858b87d7df6651040e1e08769c69b819c762
Zahra Ghandi <zahraghandi288@gmail.com>
2023-02-25 18:50:34 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Input_1 = Console.ReadLine();
+            string Input_2 = Console.ReadLine();
+            int num1 = Convert.ToInt32(Input_1);
+            int num2 = Convert.ToInt32(Input_2);
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
